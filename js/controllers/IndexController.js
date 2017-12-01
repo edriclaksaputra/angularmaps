1ae773b955d841fab1f75e50eb658d56b771244c
Edric Laksa Putra <Edric Laksa Putra>
2017-12-01 16:03:05 +0700

    Get current position using HTML geolocation

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -9,19 +9,27 @@ mapapp.controller('IndexController', ['$scope', '$state', 'IndexServices', 'NgMa
       vm.map = map;
     });
 
-  	$scope.zoom = 7;
-
 	IndexServices.listUsers().then(function(res){
 		vm.listUsers = res.data;
 		$scope.listUsers = res.data;
 	});
 
-	$scope.showCityInfo = function(event, detail){
-		alert("Username : "+detail.username+"\nName : "+detail.first_name+" "+detail.last_name+"\nEmail : "+detail.email);
-	}
-
 	$scope.init = function(){
-		
+		var confirmResult = confirm("Aplication want to get your current location, it needs your permition")
+		if(confirmResult == true){
+		    if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(showPosition);
+		    } else {
+		        $scope.center = "[0,0]";
+		    }
+			function showPosition(position) {
+			    $scope.center = "["+position.coords.latitude+","+position.coords.longitude+"]";
+			}
+		}
+		else{
+  			$scope.zoom = 10;
+			$scope.center = "[-6.922586, 107.6060501]";
+		}
 	}
 
 	$scope.goto = function(lat, long){
